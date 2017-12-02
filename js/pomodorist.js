c6afaed5cc43989fb11ac614e425f75baaa2ff44
bokalina <milosevicbojana90@gmail.com>
2017-12-02 04:33:16 -0800

    create javascript

--- js/pomodorist.js
+++ js/pomodorist.js
@@ -1,1 +1,36 @@
+var totalTime = 1500;
+var minutes = getElementById("minutes");
+var seconds = getElementById("seconds");
+var x = setInterval(count, 10);
 
+function isNegative(num) {
+    if (isNaN(num)) {
+        return false;
+    }
+    return Math.min(num, 0) != 0;
+}
+
+function count() {
+    var mins = Math.floor(totalTime / 60);
+    var secs = totalTime - mins * 60;
+    document.getElementById("minutes").innerHTML = mins;
+    document.getElementById("seconds").innerHTML = secs;
+    totalTime--;
+    console.log(totalTime)
+    if (isNegative(totalTime)) {
+        clearInterval(x);
+        message.innerHTML = "Your work time is over, take a break";
+    }
+}
+
+function start() {
+    var x = setInterval(count, 10);
+}
+
+function stop() {
+    clearInterval(x);
+}
+
+// function reset() {
+
+// }
